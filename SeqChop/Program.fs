3ba304439be04ca3d04f3677771abb7501db5a7b
Homi Chothia <homi.chothia@rakuten.com>
2015-08-20 17:45:01 -0700

    Added altenate method

--- SeqChop/Program.fs
+++ SeqChop/Program.fs
@@ -1,6 +1,8 @@
 ﻿
 module SeqChop = 
 
+    // sub sequences are captured as a list
+    // is a safe alternate since the caller cannot affect the seq of seq
     let Seqment segmentSize source = 
         let rec seqment size acc src = 
             seq {
@@ -13,15 +15,45 @@ module SeqChop =
         seqment segmentSize [] source 
 
 
+    // uses a mutable to capture the state of the sequence
+    // does not need to allocate a list but if the caller accesses the seq of seq, out of order, 
+    // the result is indeterminable 
+    let SubSequence size sequence = 
+
+        let mutable sq = sequence
+
+        let rec subSequence sz =
+            seq {
+                match not <| Seq.isEmpty sq, sz with 
+                |   false, _    
+                |   true, 0     ->  ()   
+                |   true, _     ->  yield   (Seq.head sq)
+                                    sq <- Seq.skip 1 sq
+                                    yield!  subSequence (sz - 1) 
+            } 
+
+        let rec subSequenceMain () =
+            seq {
+                match not <| Seq.isEmpty sq with 
+                |   true    ->  yield   subSequence size        // seq
+                                yield!  subSequenceMain ()      // seq of seq
+                |   false   ->  ()
+            } 
+        subSequenceMain ()
+
+
+
 [<EntryPoint>]
 let main argv = 
     
     let x = [|1; 2; 3; 4; 5; 6; 7|]
     
-    let y = SeqChop.Seqment 3 x
+    let y = SeqChop.SubSequence 3 x
 
-    y |>
-    Seq.iter
-        (printfn "%A") 
+    y 
+    |>  Seq.iter
+            (fun sub -> 
+                sub |> Seq.iter (printf "%A")
+                printfn "") 
 
     0 // return an integer exit code
